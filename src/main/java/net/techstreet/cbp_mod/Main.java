6166bf5300579811cb3b839d6a8de615c246dd57
TechStreet <dftechstreet@gmail.com>
2021-05-05 16:01:20 +0100

    Basic mod made, more soon.

---
+++ src/main/java/net/techstreet/cbp_mod/Main.java
@@ -0,0 +1,14 @@
+package net.techstreet.cbp_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    public static final String MOD_ID = "discstudiosclient";
+    public static final String MOD_NAME = "DiscStudios Client";
+    public static final String MOD_VERSION = "2.2.1";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Creative Build Plots mod has been loaded!");
+    }
+}
---
+++ src/main/java/net/techstreet/cbp_mod/mixin/screen/TitleScreenMixin.java
@@ -0,0 +1,93 @@
+package net.techstreet.cbp_mod.mixin.screen;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.RotatingCubeMapRenderer;
+import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.techstreet.cbp_mod.Main;
+import net.techstreet.cbp_mod.mixin.render.BlendableTexturedButtonWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public abstract class TitleScreenMixin extends Screen {
+    @Shadow
+    private String splashText;
+    @Shadow
+    private int copyrightTextX;
+    @Shadow
+    private static Identifier EDITION_TITLE_TEXTURE = new Identifier("textures/gui/title/edition.png");
+    @Shadow
+    private static Identifier MINECRAFT_TITLE_TEXTURE = new Identifier("textures/gui/title/minecraft.png");
+
+    private final Identifier identifier_main = new Identifier(Main.MOD_ID + ":df.png");
+    protected TitleScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/RotatingCubeMapRenderer;render(FF)V"))
+    private void render(RotatingCubeMapRenderer rotatingCubeMapRenderer, float delta, float alpha, MatrixStack matrices, int mouseX, int mouseY, float d) {
+        rotatingCubeMapRenderer.render(delta, alpha);
+    }
+
+    @Inject(at = @At("RETURN"), method = "initWidgetsNormal")
+    public void init(CallbackInfo callbackInfo) {
+        assert this.client != null;
+        this.client.options.realmsNotifications = false;
+    }
+
+    @Inject(at = @At("TAIL"), method = "init()V")
+    protected void initButtons(CallbackInfo info) {
+        /*
+        MINECRAFT_TITLE_TEXTURE = new Identifier("Minecraft", "assets/empty.png");
+        EDITION_TITLE_TEXTURE = new Identifier("JavaEdition", "assets/empty.png");
+        */
+
+        splashText = null;
+
+        copyrightTextX = 1000000000;
+
+        for (AbstractButtonWidget button : this.buttons) {
+            button.visible = false;
+        }
+
+
+        this.addButton(new ButtonWidget(this.width - (this.width - 10), this.height - (this.height - 110), 150, 20, new LiteralText("Play"), (buttonWidget) -> {
+            MinecraftClient mc = MinecraftClient.getInstance();
+            mc.openScreen(new MultiplayerScreen(mc.currentScreen));
+        }));
+
+
+        this.addButton(new ButtonWidget(this.width - (this.width - 10), this.height - (this.height - 140), 120, 20, new LiteralText("DiamondFire"), (buttonWidget) -> {
+            MinecraftClient mc = MinecraftClient.getInstance();
+            ServerInfo serverInfo = new ServerInfo("DF", "mcdiamondfire.com:25565", false);
+            mc.openScreen(new ConnectScreen(mc.currentScreen, mc, serverInfo));
+        }));
+
+        this.addButton(new ButtonWidget(this.width - (this.width - 10), this.height - (this.height - 170), 120, 20, new LiteralText("Quit Game"), (buttonWidget) -> {
+            MinecraftClient mc = MinecraftClient.getInstance();
+            System.exit(1);
+        }));
+
+        this.addButton(new BlendableTexturedButtonWidget(10, 10, 20, 20, 0, 0, 20, identifier_main, 20, 40,
+                (button) -> {
+                    MinecraftClient mc = MinecraftClient.getInstance();
+                    ServerInfo serverInfo = new ServerInfo("DF", "mcdiamondfire.com:25565", false);
+                    mc.openScreen(new ConnectScreen(mc.currentScreen, mc, serverInfo));
+                }));
+    }
+}
+
